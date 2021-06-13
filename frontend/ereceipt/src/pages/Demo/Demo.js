ead5c74254134b6c1120197065b33ff2c70c1233
Edwin Tumbaga <edwin.tumbaga@gmail.com>
2021-06-13 19:35:49 -0400

    Add Demo page (for future experimentations)

---
+++ frontend/ereceipt/src/pages/Demo/Demo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class DemoPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <p>This is a Demo Page! </p>
+        )
+    }
+}
+
+export default DemoPage
